a76bf5fc4e21b7de6da00ba84c129a4cde57a9c4
soukainaasaasslc25-boop <soukaina.asaas.slc25@gmail.com>
2025-12-25 10:52:54 +0000

    Prototype

---
+++ Prototype/Prototype.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE BlogEnLigne;
+USE BlogEnLigne;
+
+
+CREATE TABLE Utilisateur (
+    id_utilisateur INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    mot_de_passe VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE Categorie (
+    id_categorie INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL UNIQUE
+);
+
+
+CREATE TABLE Article (
+    id_article INT AUTO_INCREMENT PRIMARY KEY,
+    titre VARCHAR(200) NOT NULL,
+    contenu TEXT NOT NULL,
+    date_publication DATE NOT NULL,
+    statut ENUM('publié', 'brouillon'),
+    id_utilisateur INT NOT NULL,
+    id_categorie INT NOT NULL,
+    FOREIGN KEY (id_utilisateur) REFERENCES Utilisateur(id_utilisateur) ,
+    FOREIGN KEY (id_categorie) REFERENCES Categorie(id_categorie) 
+);
+
+
+CREATE TABLE Commentaire (
+    id_commentaire INT AUTO_INCREMENT PRIMARY KEY,
+    contenu TEXT NOT NULL,
+    date_commentaire DATETIME ,
+    id_article INT NOT NULL,
+    id_utilisateur INT NOT NULL,
+    FOREIGN KEY (id_article) REFERENCES Article(id_article) ,
+    FOREIGN KEY (id_utilisateur) REFERENCES Utilisateur(id_utilisateur)
+);
+
+
+SHOW TABLES;
+DESCRIBE Utilisateur;
+DESCRIBE Article;
